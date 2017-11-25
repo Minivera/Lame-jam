ea02bffa34d810d4146fc633d078485d88d0d57c
Minivera <guillaume.stpierre@carleton.ca>
2017-11-25 17:50:52 -0500

    Many improvments! Yay!

--- Guillaume/Scripts/Game.gd
+++ Guillaume/Scripts/Game.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var scroll_speed = 2
+var scroll_speed = 0.5
 var total_scroll = 0;
 
 func _ready():
--- Guillaume/Scripts/sides.gd
+++ Guillaume/Scripts/sides.gd
@@ -4,34 +4,40 @@ extends Node2D
 const MAX_MIDDLE_SPACE = 0.80
 const MIN_MIDDLE_SPACE = 0.50
 
-# Constants that define how close one side can be to the center, in percentages.
-const MAX_SIDE_LEAN = 0.20
+# Constants that defines how far from the last point the next point can be, in percentages.
+const MAX_SIDE_LEAN = 0.10
+
+# Constants that defines how close the two dots can be, in pixels.
+const MAX_SIDE_LEAN = 200
 
 # Saves all the dots for both sides in an array of array of vectors for the dots.
-var dots = []
+var LeftPath = []
+var RightPath = []
 
 # Accumulates speed at every call of the process method until it reaches the speed global
 var accumulation = 0
 
 func _ready():
 	set_process(true)
-	self.set_pos(Vector2(get_viewport_rect().size.width/2, get_viewport_rect().size.height/2))
+	var viewport = get_viewport().get_rect().size
+	var viewWidth = viewport[0]
+	var viewHeight = viewport[1]
+	# Get a random number for the max and min middle space
+	var sizePercent = rand_range(MIN_MIDDLE_SPACE, MAX_MIDDLE_SPACE)
+	var spotWidth = viewWidth * sizePercent
+	LeftPath.push_front(Vector2(viewWidth / 2 - spotWidth / 2, viewHeight))
+	LeftPath.push_front(Vector2(viewWidth / 2 - spotWidth / 2, 0))
+	RightPath.push_front(Vector2(viewWidth - spotWidth / 2, viewHeight))
+	RightPath.push_front(Vector2(viewWidth - spotWidth / 2, 0))
+	get_node("RightDraw").path = LeftPath;
+	get_node("RightDraw").update()
+	get_node("LeftDraw").path = RightPath;
+	get_node("LeftDraw").update()
 
 func _draw():
-	# The actual curve for the left side
-	var curveLeft = Curve2D.new()
-	# The actual curve for the right side
-	var curveRight = Curve2D.new()
-	
-	var col = Color(0,0,0,1)
-	print(dots)
-	for i in range(dots.size()):
-		if i > 0:
-			curveLeft.add_point(dots[i]["left"])
-			curveRight.add_point(dots[i]["right"])
-	get_node("RightDraw/RightSide").set_curve(curveLeft)
-	get_node("LeftDraw/LeftSide").set_curve(curveRight)
+	get_node("RightDraw").path = LeftPath;
 	get_node("RightDraw").update()
+	get_node("LeftDraw").path = RightPath;
 	get_node("LeftDraw").update()
 
 func _process(delta):
@@ -45,24 +51,18 @@ func _process(delta):
 
 func generate_middle_dot():
 	var total_scroll = get_node("/root/Game").total_scroll
-	var viewport = get_viewport().get_rect().size
-	var viewWidth = viewport[0]
-	var centerPos = viewWidth / 2
-	# Get a random number for the max and min middle space
-	var sizePercent = rand_range(MIN_MIDDLE_SPACE, MAX_MIDDLE_SPACE)
-	var spotWidth = viewWidth * sizePercent
 	# Get a random number for the distance between spot and middle (Minus mean to the left
 	var sideLean = rand_range(-MAX_SIDE_LEAN, MAX_SIDE_LEAN)
-	var finalLean = sideLean * viewWidth;
-	var dot = {
-		"left" : Vector2(centerPos - ((spotWidth / 2) + finalLean), 0),
-		"right": Vector2(centerPos + ((spotWidth / 2) + finalLean), 0)
-	}
+	var leftPos = LeftPath[0].x + (LeftPath[0].x * sideLean)
+	var rightPos = RightPath[0].x + (RightPath[0].x * sideLean)
+	var leftdot = Vector2(leftPos, 0)
+	var rightdot = Vector2(rightPos, 0)
 	# Add the dot to the array
 	# Go through all dots and add the total scroll to their position
-	for i in range(dots.size()):
+	for i in range(LeftPath.size()):
 		if i > 0:
-			dots[i]["left"] = Vector2(dots[i]["left"].x, dots[i]["left"].y + total_scroll)
-			dots[i]["right"] = Vector2(dots[i]["left"].x, dots[i]["left"].y + total_scroll)
-	dots.push_front(dot)
+			LeftPath[i] = Vector2(LeftPath[i].x, LeftPath[i].y + total_scroll)
+			RightPath[i] = Vector2(RightPath[i].x, RightPath[i].y + total_scroll)
+	LeftPath.push_front(leftdot)
+	RightPath.push_front(rightdot)
 	update()
