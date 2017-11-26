68b515305b35fd1c7e0248db92d830ab585cb0b6
ConradBelaire <cbelaire@gmail.com>
2017-11-26 12:05:54 -0500

    Fixed shines a little bit, added shinescript.gd

---
+++ Conrad/Scripts/Shinescript.gd
@@ -0,0 +1,45 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = \"textvar\"
+
+var direction = Vector2(0.0, 1.0)
+var scroll_speed
+
+
+
+func init():
+	
+	var viewport = get_viewport().get_rect().size
+	var viewWidth = viewport[0]
+	var viewHeight = viewport[1]
+	var position = get_pos()
+	
+	position.y = 0
+	position.x = round(rand_range(0.0, viewWidth))
+	set_pos(position)
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	#size = get_texture().get_size()
+    set_process(true)
+
+func _process(delta):
+	scroll_speed = get_node("/root/Game").scroll_speed
+	var pos = get_pos()
+	pos += direction * scroll_speed * delta
+	set_pos(pos)
+
+
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+#	pass
