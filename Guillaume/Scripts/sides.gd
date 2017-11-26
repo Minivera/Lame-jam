83e1598f297802c8adf396e733199f3d618252d6
Minivera <guillaume.stpierre@carleton.ca>
2017-11-25 20:19:25 -0500

    Modified borders

--- Guillaume/Scripts/sides.gd
+++ Guillaume/Scripts/sides.gd
@@ -5,10 +5,10 @@ const MAX_MIDDLE_SPACE = 0.80
 const MIN_MIDDLE_SPACE = 0.50
 
 # Constants that defines how far from the last point the next point can be, in percentages.
-const MAX_SIDE_LEAN = 0.10
+const MAX_SIDE_LEAN = 0.005
 
 # Constants that defines how close the two dots can be, in pixels.
-const MAX_SIDE_LEAN = 200
+const MIN_DISTANCE = 200
 
 # Saves all the dots for both sides in an array of array of vectors for the dots.
 var LeftPath = []
@@ -16,6 +16,12 @@ var RightPath = []
 
 # Accumulates speed at every call of the process method until it reaches the speed global
 var accumulation = 0
+
+# Current direction the walls are going
+var direction = 0
+
+var maxLeft = 0;
+var maxRight = 0;
 
 func _ready():
 	set_process(true)
@@ -25,16 +31,24 @@ func _ready():
 	# Get a random number for the max and min middle space
 	var sizePercent = rand_range(MIN_MIDDLE_SPACE, MAX_MIDDLE_SPACE)
 	var spotWidth = viewWidth * sizePercent
-	LeftPath.push_front(Vector2(viewWidth / 2 - spotWidth / 2, viewHeight))
-	LeftPath.push_front(Vector2(viewWidth / 2 - spotWidth / 2, 0))
-	RightPath.push_front(Vector2(viewWidth - spotWidth / 2, viewHeight))
-	RightPath.push_front(Vector2(viewWidth - spotWidth / 2, 0))
+	var leftWall = viewWidth / 2 - spotWidth / 2
+	# Add a straight wall to the paths
+	LeftPath.push_front(Vector2(leftWall, viewHeight))
+	LeftPath.push_front(Vector2(leftWall, 0))
+	RightPath.push_front(Vector2(leftWall + spotWidth, viewHeight))
+	RightPath.push_front(Vector2(leftWall + spotWidth, 0))
+	# Render the sides
 	get_node("RightDraw").path = LeftPath;
 	get_node("RightDraw").update()
 	get_node("LeftDraw").path = RightPath;
 	get_node("LeftDraw").update()
+	# Setup variables
+	direction = rand_range(-MAX_SIDE_LEAN, MAX_SIDE_LEAN)
+	maxLeft = 0 + viewWidth * (1 - MAX_MIDDLE_SPACE)
+	maxRight = viewWidth - viewWidth * (1 - MAX_MIDDLE_SPACE)
 
 func _draw():
+	# Render the sides
 	get_node("RightDraw").path = LeftPath;
 	get_node("RightDraw").update()
 	get_node("LeftDraw").path = RightPath;
@@ -47,14 +61,22 @@ func _process(delta):
 	var speed = get_node("/root/Game").scroll_speed
 	if (accumulation >= speed):
 		accumulation = 0
+		# Get a random number for the distance between spot and middle (Minus means to the left)
+		direction = rand_range(0 - MAX_SIDE_LEAN, MAX_SIDE_LEAN)
+		generate_middle_dot()
+	else:
 		generate_middle_dot()
 
 func generate_middle_dot():
+	var viewport = get_viewport().get_rect().size
 	var total_scroll = get_node("/root/Game").total_scroll
-	# Get a random number for the distance between spot and middle (Minus mean to the left
-	var sideLean = rand_range(-MAX_SIDE_LEAN, MAX_SIDE_LEAN)
-	var leftPos = LeftPath[0].x + (LeftPath[0].x * sideLean)
-	var rightPos = RightPath[0].x + (RightPath[0].x * sideLean)
+	
+	var leftPos = max(LeftPath[0].x + (LeftPath[0].x * direction), maxLeft)
+	var rightPos = min(RightPath[0].x + (RightPath[0].x * direction), maxRight)
+	#print(leftPos - rightPos)
+	#if (leftPos - rightPos <= MIN_DISTANCE):
+		#rightPos += leftPos - rightPos / 2
+		#leftPos += leftPos - rightPos / 2
 	var leftdot = Vector2(leftPos, 0)
 	var rightdot = Vector2(rightPos, 0)
 	# Add the dot to the array
