6699f2b314bc8faffc2ebd2d629f32faa0af2795
Minivera <guillaume.stpierre@carleton.ca>
2017-11-26 15:55:16 -0500

    Collision IUjhsjhdagjkfdgjhasfhgsadffdasjkysadghsdafhg

--- Conrad2/Scripts/player_script.gd
+++ Conrad2/Scripts/player_script.gd
@@ -51,17 +51,17 @@ func _process(delta):
 		sun_frame = 10
 	set_frame(sun_frame)
 	
-	if(Input.is_action_pressed("turn_right") and not direction.x > 0.8 and speed_adjust_ready):
+	if(Input.is_action_pressed("turn_right") and not direction.x > 0.8 and speed_adjust_ready and not dead):
 		direction.x += turnspeed
 		speed_adjust_ready = false
 		input_timer.start()
 
-	if(Input.is_action_pressed("turn_left") and not direction.x < -0.8 and speed_adjust_ready):
+	if(Input.is_action_pressed("turn_left") and not direction.x < -0.8 and speed_adjust_ready and not dead):
 		direction.x -= turnspeed
 		speed_adjust_ready = false
 		input_timer.start()
 
-	if(Input.is_action_pressed("correct_vertical") and not Input.is_action_pressed("turn_left") and not Input.is_action_pressed("turn_right") and speed_adjust_ready):
+	if(Input.is_action_pressed("correct_vertical") and not Input.is_action_pressed("turn_left") and not Input.is_action_pressed("turn_right") and speed_adjust_ready and not dead):
 		if(direction.x < 0):
 			direction.x += turnspeed
 		if(direction.x > 0):
@@ -75,6 +75,7 @@ func _process(delta):
 		segment_drop_ready = false
 		segment_timer.start()
 	if(dead):
+		direction.x = 0
 		segment_drop_ready = false
 		speed_adjust_ready = false
 		get_node("/root/Game").scroll_speed = 0
@@ -88,6 +89,25 @@ func _process(delta):
 	else:
 		scroll_speed = sqrt(abs(100.0*100.0 - (abs(direction.x) * 100.0 - 10) * (abs(direction.x) * 100.0 - 10)))
 		get_node("/root/Game").scroll_speed = scroll_speed
-	
+	detect_wall_collision()
 
-	
+func detect_wall_collision() :
+	var playerRect
+	if (sun_frame > 5) :
+		playerRect = Rect2(Vector2(pos.x - 32, pos.y - 32), Vector2(32, 32))
+	else :
+		playerRect = Rect2(Vector2(pos.x + 48, pos.y + 48), Vector2(24, 24))
+	var leftWall = get_node("/root/Game/Sides").LeftPath
+	var rightWall = get_node("/root/Game/Sides").RightPath
+	var viewport = get_viewport().get_rect().size
+	var viewWidth = viewport[0]
+	for i in range (leftWall.size()):
+		if i > 0:
+			var rect = Rect2(0, leftWall[i].y, leftWall[i].x, 2)
+			if (rect.intersects(playerRect)):
+				dead = true
+	for i in range (rightWall.size()):
+		if i > 0:
+			var rect = Rect2(rightWall[i-1].x, rightWall[i].y, viewWidth, 2)
+			if (rect.intersects(playerRect)):
+				dead = true
