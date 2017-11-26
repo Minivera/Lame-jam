3da7a7c55dc1de3b3dba3a6a0db3ca6add9580d2
Minivera <guillaume.stpierre@carleton.ca>
2017-11-25 21:16:39 -0500

    Smooth walls!

--- Guillaume/Scripts/sides.gd
+++ Guillaume/Scripts/sides.gd
@@ -5,10 +5,14 @@ const MAX_MIDDLE_SPACE = 0.80
 const MIN_MIDDLE_SPACE = 0.50
 
 # Constants that defines how far from the last point the next point can be, in percentages.
-const MAX_SIDE_LEAN = 15
+const MAX_SIDE_LEAN = 5
+
+# How many time should the random try to go in the same direction to make it "smooth"
+const TIME_SAME_SIDE = 15
+var timesSameSide = 0
 
 # Acceleration when moving to one side
-const SIDE_ACCELERATION = 0.20
+const SIDE_ACCELERATION = 0.5
 
 # Constants that defines how close the two dots can be, in pixels.
 const MIN_DISTANCE = 400
@@ -17,11 +21,13 @@ const MIN_DISTANCE = 400
 var LeftPath = []
 var RightPath = []
 
-# Accumulates speed at every call of the process method until it reaches the speed global
-var accumulation = 0
-
 # Current direction the walls are going
 var direction = {
+	"left" : 0,
+	"right" : 0
+}
+
+var currentLean = {
 	"left" : 0,
 	"right" : 0
 }
@@ -62,30 +68,40 @@ func _draw():
 	get_node("LeftDraw").update()
 
 func _process(delta):
-	accumulation += delta
 	# TODO: REMOVE THAT!
 	get_node("/root/Game").total_scroll += delta
-	var speed = get_node("/root/Game").scroll_speed
-	if (accumulation >= speed):
-		accumulation = 0
-		# Get a random number for the distance between spot and middle (Minus means to the left)
-		direction["left"] = rand_range(0 - MAX_SIDE_LEAN, MAX_SIDE_LEAN)
-		direction["right"] = rand_range(0 - MAX_SIDE_LEAN, MAX_SIDE_LEAN)
-		generate_middle_dot(delta)
+	if (currentLean["left"] >= abs(direction["left"]) or currentLean["right"] >= abs(direction["right"])):
+		print("rcalcul")
+		# Check if we are still going to the same side
+		if (timesSameSide == TIME_SAME_SIDE) :
+			timesSameSide = 0
+			# Get a random number for the distance between spots and middle (Minus means to the left)
+			direction["left"] = rand_range(0 - MAX_SIDE_LEAN, MAX_SIDE_LEAN)
+			direction["right"] = rand_range(0 - MAX_SIDE_LEAN, MAX_SIDE_LEAN)
+		else :
+			timesSameSide += 1
+			# If yes, contnue going that side
+			direction["left"] = rand_range(0, sign(direction["left"]) * MAX_SIDE_LEAN)
+			direction["right"] = rand_range(0, sign(direction["right"]) * MAX_SIDE_LEAN)
+		currentLean["left"] = 0
+		currentLean["right"] = 0
+		generate_middle_dot()
 	else:
-		generate_middle_dot(delta)
+		generate_middle_dot()
 
-func generate_middle_dot(delta):
+func generate_middle_dot():
 	var viewport = get_viewport().get_rect().size
 	var total_scroll = get_node("/root/Game").total_scroll
 	
-	var leftPos = max(LeftPath[0].x + ((direction["left"] + delta) * SIDE_ACCELERATION), maxLeft)
-	var rightPos = min(RightPath[0].x + ((direction["right"] + delta) * SIDE_ACCELERATION), maxRight)
+	currentLean["left"] += abs(direction["left"] * SIDE_ACCELERATION)
+	currentLean["right"] += abs(direction["right"] * SIDE_ACCELERATION)
+	var leftPos = max(LeftPath[0].x + direction["left"] * SIDE_ACCELERATION, rand_range(maxLeft, maxLeft - 40))
+	var rightPos = min(RightPath[0].x + direction["right"] * SIDE_ACCELERATION, rand_range(maxRight, maxRight - 40))
 	print(rightPos - leftPos)
 	print(MIN_DISTANCE)
 	if (rightPos - leftPos <= MIN_DISTANCE):
-		leftPos = max(LeftPath[0].x + ((-direction["left"] + delta) * SIDE_ACCELERATION), maxLeft)
-		rightPos = min(RightPath[0].x + ((-direction["right"] + delta) * SIDE_ACCELERATION), maxRight)
+		leftPos = max(LeftPath[0].x + ((-direction["left"]) * SIDE_ACCELERATION), maxLeft)
+		rightPos = min(RightPath[0].x + ((-direction["right"]) * SIDE_ACCELERATION), maxRight)
 	var leftdot = Vector2(leftPos, 0)
 	var rightdot = Vector2(rightPos, 0)
 	# Add the dot to the array
