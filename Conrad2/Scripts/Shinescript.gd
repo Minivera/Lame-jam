d5199231a66a65f5cb987b678fbd27e89d183fa9
Minivera <guillaume.stpierre@carleton.ca>
2017-11-26 15:30:54 -0500

    Remove stuffffffff

--- Conrad2/Scripts/Shinescript.gd
+++ Conrad2/Scripts/Shinescript.gd
@@ -47,14 +47,3 @@ func _process(delta):
 		self.hide()
 		self.queue_free()
 
-
-
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-#func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-#	pass
